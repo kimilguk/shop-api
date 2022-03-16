e87a40501b1c242db0431f1d3a66387d48b0aae4
kimilguk <boramcom@daum.net>
2022-03-16 18:06:39 +0900

    20220315(화) 작업소스 스프링부트API서비스(서버) - 안드로이드 앱(클라이언트) 통신을 사용

--- src/main/kotlin/org/kimilguk/shop/common/ApiResponse.kt
+++ src/main/kotlin/org/kimilguk/shop/common/ApiResponse.kt
@@ -8,7 +8,7 @@ data class ApiResponse(
     val data: Any? = null,
     val message: String? = null
 ) {
-    companion object {//static 대신 companion 을 사용, object는 싱그론 클래스로 실행과 동시에 1개의 객체가 생성된다.
+    companion object {//static 대신 companion 을 사용, object는 싱글톤 클래스로 실행과 동시에 1개의 객체가 생성된다.
         fun ok(data: Any? = null) = ApiResponse(true, data) //ok 일때 data 를 전송받고
         fun error(message: String? = null) = ApiResponse(false, message = message)//false 앨때 에러메세지를 전송받는다.
     }
